f9128795a55fb745357876cd4acbd5cf9c4fc453
jun17183 <64164782+jun17183@users.noreply.github.com>
2023-10-23 00:40:11 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = 0;
+
+    let str = n.toString();
+    
+    for (let i = 0; i < str.length; i++) {
+        answer += Number(str[i])
+    }
+    
+    return answer;
+}

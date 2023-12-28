6b79075191a767a3b0e724ec6e7df3240c798782
jun17183 <64164782+jun17183@users.noreply.github.com>
2023-12-28 12:04:56 +0900

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,4 +1,6 @@
 // 매 회차마다 i와 i+1을 비교하여 큰 수를 뒤로 보내어 가장 큰 수부터 뒤로 보내는 정렬
+// 시간 복잡도는 n^2이며 같은 n^2 정렬인 선택 정렬, 삽입 정렬과 비교하여도 효율이 가장 좋지 못하다.
+// 이유는 매 비교마다 자리를 바꾸기 때문
 function bubbleSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i; j++) {

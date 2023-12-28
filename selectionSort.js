23b5616283659fe504ffa6d4c1c3c5b70991af63
jun17183 <64164782+jun17183@users.noreply.github.com>
2023-12-28 11:34:14 +0900

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -1,5 +1,6 @@
 // n번째 회차마다 n번째로 작은 수를 찾아 정렬
 // 모든 인덱스와 비교하며 가장 작은 수의 인덱스를 저장하였다가 검사가 끝나면 스왑한다.
+// 시간 복잡도는 n^2로 비효율적인 정렬 방법이다.
 function selectionSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
         let idx = i;

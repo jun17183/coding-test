ea23ab6aa437de393047eaa48c17af1d2740857f
jun17183 <64164782+jun17183@users.noreply.github.com>
2024-02-20 23:31:01 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -2,18 +2,16 @@ let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let answer = 0;
-let money = Number(input[0].split(" ")[1]);
+let [N, K] = input[0].split(" ");
+N = +N;
+K = +K;
 
-for (let i = input.length - 1; i > 0; i--) {
-    let coin = Number(input[i]);
-    if (coin > money) continue;
+for (let i = N; i >= 1; i--) {
+    let coin = +input[i];
+    if (coin > K) continue;
 
-    answer += Math.floor(money / coin);
-    
-    let rest = money % coin;
-    if (rest === 0) break;
-
-    money = rest;
+    answer += Math.floor(K / coin);
+    K = K % coin;
 }
 
 console.log(answer);

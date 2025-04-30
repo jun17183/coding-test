9aa3e9bb02bde87a6d0689e28b23f0fec45ebab9
jun17183 <64164782+jun17183@users.noreply.github.com>
2025-04-30 13:44:51 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 160 ms, Memory: 9660 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = Number(input[0]);
+const A = input[1].split(' ').map(Number);
+const dp = Array(n).fill(1);
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        if (A[j] < A[i]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+    }
+}
+
+console.log(Math.max(...dp));

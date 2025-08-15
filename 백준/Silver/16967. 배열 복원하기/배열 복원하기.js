6be1b65184940f3d8284b4144ebd02a7f6c6acc3
jun17183 <64164782+jun17183@users.noreply.github.com>
2025-08-15 23:53:36 +0900

    [Silver III] Title: 배열 복원하기, Time: 196 ms, Memory: 20988 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [H, W, X, Y] = input[0].split(" ").map(Number);
+const B = input.slice(1).map(line => line.split(" ").map(Number));
+const A = Array.from({ length: H }, () => Array(W).fill(0));
+
+for (let i = 0; i < H; i++) {
+  for (let j = 0; j < W; j++) {
+    if (i >= X && j >= Y) {
+      A[i][j] = B[i][j] - A[i - X][j - Y];
+    } else {
+      A[i][j] = B[i][j];
+    }
+  }
+}
+
+console.log(A.map(row => row.join(" ")).join("\n"));
